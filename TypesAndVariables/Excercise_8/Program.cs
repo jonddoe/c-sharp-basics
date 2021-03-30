27453654d0ef45a6e7183824417eda48b8349784
jonddoe <jandirkalman@gmail.com>
2021-03-31 00:52:57 +0300

    Excercise_8 done

---
+++ TypesAndVariables/Excercise_8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Excercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter minutes:");
+            var minutes = Int64.Parse(Console.ReadLine());
+            var years = minutes / 525600;
+            var days = minutes / 1440;
+            var remainingMinutesInYears = days % 525600;
+            var remainingDaysInYears= (remainingMinutesInYears / 1440).ToString("N0");
+            Console.WriteLine($"{minutes} minutes equals {years} years or {days} days. Or {years} years and {remainingDaysInYears} days.");
+        }
+    }
+}
