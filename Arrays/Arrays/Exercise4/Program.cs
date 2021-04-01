ae23e03cb49627c3201401a0ab6b99acf3b2016f
jonddoe <jandirkalman@gmail.com>
2021-04-01 12:08:47 +0300

    Exercise4 done

--- Arrays/Arrays/Exercise4/Program.cs
+++ Arrays/Arrays/Exercise4/Program.cs
@@ -11,19 +11,23 @@ namespace Exercise4
         //TODO: Write a C# program to test if an array contains a specific value.
         private static void Main(string[] args)
         {
-            int[] myArray = {
+            int[] myArray =
+            {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2265, 1457, 2456
             };
 
-            /*
-            fixme - check if contains number 1245 
-            for (?) {
-                ......
+            const int valueToCheck = 1245;
+            foreach (var i in myArray)
+            {
+                if (i != valueToCheck) continue;
                 Console.WriteLine("Contains!");
+                Console.ReadLine();
             }
-            */
+
+            Console.WriteLine("Does not contain!");
+            Console.ReadLine();
         }
     }
 }
