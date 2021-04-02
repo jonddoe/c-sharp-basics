761ba9a6b66851150813f39874b4b3c18113beed
jonddoe <jandirkalman@gmail.com>
2021-04-02 15:40:22 +0300

    Exercise_1 done

---
+++ Arithmetic/Exercise_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int.TryParse(Console.ReadLine(), out var num);
+            Console.Write("Enter another number: ");
+            int.TryParse(Console.ReadLine(), out var num2);
+
+            Boolean isFifteen()
+            {
+                if (num == 15 || num2 == 15 || num + num2 == 15 || num - num2 == 15 || num2 - num == 15)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            Console.WriteLine(isFifteen());
+            
+        }
+    }
+}
