f0ec3f60719a2505c18360479608abcf292d8a80
jonddoe <jandirkalman@gmail.com>
2021-04-04 01:09:37 +0300

    GravityCalculator done

--- Arithmetic/GravityCalculator/Program.cs
+++ Arithmetic/GravityCalculator/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 
-namespace GravityCalculator
+namespace Exercise_7
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double gravity = -9.81;  // Earth's gravity in m/s^2
-            double initialVelocity = 0.0;
-            double fallingTime = 10.0;
-            double initialPosition = 0.0;
-            double finalPosition = 0.0;
+            var gravity = -9.81;
+            var initialVelocity = 0.0;
+            var fallingTime = 10.0;
+            var initialPosition = 0.0;
+            var finalPosition = (gravity * (Math.Pow(fallingTime, 2) / 2) + (initialVelocity * fallingTime) +
+                                 initialPosition);
             Console.WriteLine("The object's position after " + fallingTime + " seconds is " + finalPosition + " m.");
             Console.ReadKey();
         }
