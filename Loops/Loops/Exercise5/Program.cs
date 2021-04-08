27d45552eb97e8ee416c87b442941740bab71739
jonddoe <jandirkalman@gmail.com>
2021-04-08 14:03:05 +0300

    Exercise5 done

---
+++ Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outputLength = 30;
+            Console.WriteLine("Enter the first word: ");
+            var firstWord = Console.ReadLine();
+            Console.WriteLine("Enter the second word: ");
+            var secondWord = Console.ReadLine();
+            var separator = "";
+            for (var i = outputLength; i > firstWord.Length + secondWord.Length; i--)
+            {
+                separator += ".";
+            }
+
+            Console.WriteLine($"{firstWord}{separator}{secondWord}");
+            Console.ReadLine();
+        }
+    }
+}
