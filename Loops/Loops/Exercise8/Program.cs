385cac66f34f1c3d2df989aec71dbfcaacf7294f
jonddoe <jandirkalman@gmail.com>
2021-04-09 17:53:11 +0300

    Exercise8 added

---
+++ Loops/Loops/Exercise8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AsciiFigure());
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+
+        static string AsciiFigure()
+        {
+            int num = 5;
+            var figure = "";
+            for (int i = 1; i <= (num - 1); i++)
+            {
+                if (i==1)
+                {
+                    for (int j = 1; j <= ((num) * (num))-1; j++)
+                    {
+                        figure += "/";
+
+                    }
+                    for (int l = 1; l <= ((num) * (num)) -1; l++)
+                    {
+                        figure += "\\";
+
+                    }
+                    figure += "\n";
+                }
+
+                for (int j = 1; j <= ((num) * (num )) - ((i * (num)) )-1; j++)
+                {
+                    figure += "/";
+
+                }
+
+                for (int k = 1; k < (((num ) * i) * 2); k++)
+                {
+                    figure += "*";
+                }
+                for (int l = 1; l <= (((num) * (num)) - (i * num)); l++)
+                {
+                    figure += "\\" ;
+
+                }
+
+                figure += "\n";
+            }
+
+            return figure;
+        }
+    }
+
+}
