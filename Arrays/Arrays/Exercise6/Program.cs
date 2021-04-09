6b01fbe68514da24e5226731967bd4eba740d5e2
jonddoe <jandirkalman@gmail.com>
2021-04-09 20:18:04 +0300

    Exercise6 fixed

--- Arrays/Arrays/Exercise6/Program.cs
+++ Arrays/Arrays/Exercise6/Program.cs
@@ -8,7 +8,6 @@ namespace Exercise6
         static void Main(string[] args)
         {
             var random = new Random();
-
             var randomValueArray1 = new int[10];
             var copyOfRandomValueArray = new int[10];
             for (var i = 0; i < randomValueArray1.Length; i++)
@@ -19,17 +18,10 @@ namespace Exercise6
 
             for (var i = 0; i < randomValueArray1.Length; i++)
             {
-                for (var j = 0; j < 1; j++)
-                {
-                    copyOfRandomValueArray[i] = randomValueArray1[i];
-                }
+                copyOfRandomValueArray[i] = randomValueArray1[i];
             }
 
-
-            for (var i = randomValueArray1.Length; i == randomValueArray1.Length; i++)
-            {
-                randomValueArray1[^1] = -7;
-            }
+            randomValueArray1[^1] = -7;
 
             Console.WriteLine($"Array 1: {string.Join(" ", randomValueArray1)}");
             Console.WriteLine($"Array 2: {string.Join(" ", copyOfRandomValueArray)}");
