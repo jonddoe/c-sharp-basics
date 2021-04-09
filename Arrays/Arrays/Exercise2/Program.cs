23b8b3c58c440c740fa7663b664df623d7d017ec
jonddoe <jandirkalman@gmail.com>
2021-04-09 19:07:06 +0300

    Exercise2 cleaned up

--- Arrays/Arrays/Exercise2/Program.cs
+++ Arrays/Arrays/Exercise2/Program.cs
@@ -5,8 +5,6 @@ namespace Exercise2
 {
     class Program
     {
-        
-        //TODO: Write a C# program to sum values of an array.
         private static void Main(string[] args)
         {
             int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
