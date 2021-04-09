f6b64e330c45eb44d62b5824189e76349c378c24
jonddoe <jandirkalman@gmail.com>
2021-04-09 20:11:15 +0300

    Exercise5 cleaned up

--- Arrays/Arrays/Exercise5/Program.cs
+++ Arrays/Arrays/Exercise5/Program.cs
@@ -8,14 +8,11 @@ namespace Exercise5
 {
     class Program
     {
-        //TODO: Write a C# program to find the index of an array element.
         private static void Main(string[] args)
         {
             int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
-
             var indexOfElement36 = Array.IndexOf(myArray, 36);
             var indexOfElement29 = Array.IndexOf(myArray, 29);
-
 
             Console.WriteLine("Index position of 36 is: " + indexOfElement36);
             Console.WriteLine("Index position of 29 is: " + indexOfElement29);
