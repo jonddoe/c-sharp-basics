453827969a89c87de17d7bbcfd552e7202946159
jonddoe <jandirkalman@gmail.com>
2021-04-11 18:58:15 +0300

    Exercise_1 done

---
+++ ClassesAndObjects/Exercise_1/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise_1
+{
+    public class Product
+   {
+       private string _name;
+       private int _amount { get; set; }
+       private double _price { get; set; }
+       public Product(string name, double priceAtStart, int amountAtStart)
+       {
+           _name = name;
+           _amount = amountAtStart;
+           _price = priceAtStart;
+
+       }
+        public void ChangeAmount(int amount)
+        {
+            _amount = amount;
+        }
+        public void ChangePrice(double price)
+        {
+            _price = price;
+        }
+        public string PrintProduct()
+        {
+            return $"{_name}, {_price} EUR, {_amount} Units";
+        }
+        
+    }
+}
---
+++ ClassesAndObjects/Exercise_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product("Logitech mouse", 70.00, 14);
+            var phone = new Product("iPhone 5s", 999.99, 3);
+            var printer = new Product("Epson EB-U05", 440.46, 1);
+
+            Console.WriteLine(mouse.PrintProduct());
+            Console.WriteLine(phone.PrintProduct());
+            Console.WriteLine(printer.PrintProduct());
+            Console.ReadLine();
+
+            mouse.ChangePrice(99.99);
+            mouse.ChangeAmount(44);
+
+            phone.ChangePrice(889.99);
+            phone.ChangeAmount(74);
+
+            printer.ChangePrice(290.95);
+            printer.ChangeAmount(240);
+
+            Console.WriteLine(mouse.PrintProduct());
+            Console.WriteLine(phone.PrintProduct());
+            Console.WriteLine(printer.PrintProduct());
+        }
+    }
+}
