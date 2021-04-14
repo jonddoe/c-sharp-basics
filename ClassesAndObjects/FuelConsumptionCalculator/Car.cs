2abd7ee5d72adf1009df95b5c7330a97e3ac71e9
jonddoe <jandirkalman@gmail.com>
2021-04-14 14:07:37 +0300

    FuelConsumptionCalculator done

--- ClassesAndObjects/FuelConsumptionCalculator/Car.cs
+++ ClassesAndObjects/FuelConsumptionCalculator/Car.cs
@@ -1,20 +1,24 @@
-﻿namespace FuelConsumptionCalculator
+﻿using System;
+
+namespace FuelConsumptionCalculator
 {
     public class Car
     {
         public Car(double startOdo)
         {
-            
         }
+
+        private int _liters;
+        private int _startKilometers;
 
         public double CalculateConsumption()
         {
-            return 0;
+            return Convert.ToInt64(_startKilometers / _liters);
         }
 
         private double ConsumptionPer100Km()
         {
-            return 0;
+            return 100.0 / (Convert.ToInt64(_startKilometers / _liters));
         }
 
         public bool GasHog()
@@ -37,8 +41,10 @@
             return false;
         }
 
-        public void FillUp(int mileage, double liters)
+        public void FillUp(int mileage, int liters)
         {
+            _startKilometers = mileage;
+            _liters = liters;
         }
     }
 }
--- ClassesAndObjects/FuelConsumptionCalculator/Program.cs
+++ ClassesAndObjects/FuelConsumptionCalculator/Program.cs
@@ -10,31 +10,36 @@ namespace FuelConsumptionCalculator
     {
         private static void Main(string[] args)
         {
-            int startKilometers;
-            int liters;
-
-            
             Console.WriteLine();
 
             Car car = new Car(0);
             Car car1 = new Car(0);
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write("Enter first reading: ");
-                startKilometers = Convert.ToInt32(Console.ReadLine());    
-                Console.Write("Enter liters reading: ");
-                liters = Convert.ToInt32(Console.ReadLine());
-                car.FillUp(startKilometers, liters);
-                
-                Console.Write("Enter first reading: ");
-                startKilometers = Convert.ToInt32(Console.ReadLine());    
-                Console.Write("Enter liters reading: ");
-                liters = Convert.ToInt32(Console.ReadLine());
-                car1.FillUp(startKilometers, liters);
-            }
+            Car car2 = new Car(0);
 
-            Console.WriteLine("Kilometers per liter are " + car.CalculateConsumption() + " gasHog:" + car.GasHog());
-            Console.WriteLine("Car1 Kilometers per liter are " + car1.CalculateConsumption()+ " economyCar:" + car.EconomyCar());
+            Console.Write("Enter first reading: ");
+            int.TryParse(Console.ReadLine(), out var startKilometers);
+            Console.Write("Enter liters reading: ");
+            int.TryParse(Console.ReadLine(), out var liters);
+            car.FillUp(startKilometers, liters);
+
+            Console.Write("Enter first reading: ");
+            startKilometers = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter liters reading: ");
+            liters = Convert.ToInt32(Console.ReadLine());
+            car1.FillUp(startKilometers, liters);
+
+            Console.Write("Enter first reading: ");
+            startKilometers = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter liters reading: ");
+            liters = Convert.ToInt32(Console.ReadLine());
+            car2.FillUp(startKilometers, liters);
+
+            Console.WriteLine("Car Kilometers per liter are " + car.CalculateConsumption() + " gasHog:" + car.GasHog() +
+                              " economyCar:" + car.EconomyCar());
+            Console.WriteLine("Car1 Kilometers per liter are " + car1.CalculateConsumption() + " gasHog:" +
+                              car1.GasHog() + " economyCar:" + car1.EconomyCar());
+            Console.WriteLine("Tesla Kilometers per liter are " + car2.CalculateConsumption() + " gasHog:" +
+                              car2.GasHog() + " economyCar:" + car2.EconomyCar());
             Console.ReadKey();
         }
     }
