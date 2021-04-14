9f4d53cd71d8b3514f47f0a156843c4d5f859958
jonddoe <jandirkalman@gmail.com>
2021-04-14 15:10:39 +0300

    exercise 3 done

--- ClassesAndObjects/ConsoleApp1/FuelGauge.cs
+++ ClassesAndObjects/ConsoleApp1/FuelGauge.cs
@@ -11,12 +11,16 @@
 
         public void Fill()
         {
-            
+            if (_fuelLevel < 70)
+            {
+                _fuelLevel++;
+            }
+           
         }
 
         public void DecreaseLevel()
         {
-            
+            _fuelLevel--;
         }
     }
 }
--- ClassesAndObjects/ConsoleApp1/Odometer.cs
+++ ClassesAndObjects/ConsoleApp1/Odometer.cs
@@ -6,17 +6,31 @@ namespace ConsoleApp1
     {
         public Odometer(FuelGauge fuelGauge)
         {
-            
+            _fuelGauge = fuelGauge;
         }
+
+        private readonly FuelGauge _fuelGauge;
+        private int _currentMileage;
 
         public int Report()
         {
-            return 0;
+            return _currentMileage;
         }
 
         public void Increment()
         {
+            if (_fuelGauge.ReportLevel() <= 0) return;
+            if (_currentMileage == 1000000)
+            {
+                _currentMileage = 0;
+            }
 
+            _currentMileage++;
+
+            if (_currentMileage % 10 == 0)
+            {
+                _fuelGauge.DecreaseLevel();
+            }
         }
     }
 }
