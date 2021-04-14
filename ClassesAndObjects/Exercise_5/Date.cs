9c8471307c3731bea652e01aba352edb6c6ab706
jonddoe <jandirkalman@gmail.com>
2021-04-14 17:09:32 +0300

    Exercise_5 done

---
+++ ClassesAndObjects/Exercise_5/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Date
+    {
+        private int _month;
+        private int _day;
+        private int _year;
+
+        public void SetDate(int d, int m, int y)
+        {
+            _month = m;
+            _day = d;
+            _year = y;
+        }
+
+        public string DisplayDate()
+        {
+            return $"{_day}/{_month}/{_year}";
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day: ");
+            int.TryParse(Console.ReadLine(), out var d);
+            Console.WriteLine("Enter month: ");
+            int.TryParse(Console.ReadLine(), out var m);
+            Console.WriteLine("Enter year: ");
+            int.TryParse(Console.ReadLine(), out var y);
+
+            Date DateTest = new Date();
+
+            DateTest.SetDate(d, m, y);
+            Console.WriteLine(DateTest.DisplayDate());
+        }
+    }
+}
