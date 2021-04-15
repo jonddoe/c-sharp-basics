28dc4ac5ae7c869d463d003ec0c771abd4d21351
jonddoe <jandirkalman@gmail.com>
2021-04-16 02:06:14 +0300

    Exercise_9 done

---
+++ ClassesAndObjects/Exercise_9/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise_9
+{
+    class Point
+    {
+        public int X { set; get; }
+        public int Y { set; get; }
+
+        public static void SwapPoints(Point x, Point y)
+        {
+            var tempXx = x.X;
+            var tempXy = x.Y;
+            x.X = y.X;
+            x.Y = y.Y;
+            y.X = tempXx;
+            y.Y = tempXy;
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise_9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point {X = 5, Y = 2};
+
+            var p2 = new Point {X = -3, Y = 6};
+
+            Point.SwapPoints(p1, p2);
+            Console.WriteLine("(" + p1.X + ", " + p1.Y + ")");
+            Console.WriteLine("(" + p2.X + ", " + p2.Y + ")");
+        }
+    }
+}
