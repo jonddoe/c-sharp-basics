f76b41e248ed26e6d64636036c3d391195d1d5b4
jonddoe <jandirkalman@gmail.com>
2021-04-16 16:03:32 +0300

    BankAccount done

---
+++ ClassesAndObjects/BankAccount/BankAcc.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace BankAccount
+{
+    class BankAcc
+    {
+        private readonly double _balance = -17.25;
+        private readonly string _name = "Benson";
+
+        public string ShowUserNameAndBalance()
+        {
+            if (_balance < 0)
+            {
+                return _name + ", -$" + (Math.Abs(_balance).ToString("F2"));
+            }
+           
+            return _name + ", $" + _balance.ToString("F2");
+        }
+    }
+}
--- ClassesAndObjects/BankAccount/Program.cs
+++ ClassesAndObjects/BankAccount/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankAccount
 {
@@ -10,6 +6,9 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+            var benben = new BankAcc();
+            Console.WriteLine(benben.ShowUserNameAndBalance());
+            Console.ReadLine();
         }
     }
 }
