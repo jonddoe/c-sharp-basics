8b76afd9c4c0cad76fff733d9f5fb4e532a59498
jonddoe <jandirkalman@gmail.com>
2021-04-23 19:27:18 +0300

    ListExercise7 done

--- Collections/ListExercise7/Program.cs
+++ Collections/ListExercise7/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise7
 {
@@ -19,12 +16,8 @@ namespace ListExercise7
                 "Black"
             };
 
-            //      TODO: Check if list contains "White" element
-            //        if (...) {
-            //            Console.WriteLine("Found the element");
-            //        } else {
-            //            Console.WriteLine("There is no such element");
-            //        }
+            Console.WriteLine(colors.Contains("White") ? "Found the element" : "There is no such element");
+            Console.Read();
         }
     }
 }
