776b165a92ce595c4c52367a91be7bf155e562c0
jonddoe <jandirkalman@gmail.com>
2021-04-23 19:23:04 +0300

    ListExercise3 done

--- Collections/ListExercise3/Program.cs
+++ Collections/ListExercise3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise3
 {
@@ -10,8 +7,6 @@ namespace ListExercise3
     {
         static void Main(string[] args)
         {
-            //TODO: Write a C# program to insert an element into the array list at the first position.
-
             var colors = new List<string>();
             colors.Add("Red");
             colors.Add("Green");
@@ -21,10 +16,13 @@ namespace ListExercise3
 
             Console.WriteLine(string.Join(",", colors));
 
-            //TODO: Add new color at the first and third position of the list
+            colors.RemoveAt(0);
+            colors.Insert(0, "Grey");
+            colors.RemoveAt(2);
+            colors.Insert(2, "Blue");
 
-            //Print the list
             Console.WriteLine(string.Join(",", colors));
+            Console.Read();
         }
     }
 }
