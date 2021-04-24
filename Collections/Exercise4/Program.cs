d3a2530b818f194e5b8f720acbe44d105e8d830f
jonddoe <jandirkalman@gmail.com>
2021-04-24 14:46:47 +0300

    Exercise4 done

---
+++ Collections/Exercise4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameList = new HashSet<string>();
+            var printNamesEntered = false;
+            while (printNamesEntered == false)
+            {
+                Console.WriteLine("Enter a name:");
+                var name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name))
+                {
+                    printNamesEntered = true;
+                }
+
+                nameList.Add(name);
+            }
+
+            Console.Write("Unique name list contains: ");
+            foreach (var c in nameList)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.Read();
+        }
+    }
+}
