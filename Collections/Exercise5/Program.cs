f12c8ea0923fdf8b4d2ecad5104bf96ee6ca86fb
jonddoe <jandirkalman@gmail.com>
2021-04-27 00:56:35 +0300

    Exercise5 done

---
+++ Collections/Exercise5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int.TryParse(Console.ReadLine(), out var inputNum);
+
+            static int Factorial(string n)
+            {
+                var d = 0;
+
+                var digitsArray = n.ToCharArray();
+                foreach (var c in digitsArray)
+                {
+                    d += int.Parse(c.ToString()) * int.Parse(c.ToString());
+                }
+
+                return d;
+            }
+
+            static bool IsHappy(int n)
+            {
+                int fast;
+                var slow = fast = n;
+                do
+                {
+                    slow = Factorial(slow.ToString());
+                    fast = Factorial(Factorial(fast.ToString()).ToString());
+                } while (slow != fast);
+
+                return (slow == 1);
+            }
+
+            Console.Write(IsHappy(inputNum) ? "Happy" : "Not Happy");
+            Console.Read();
+        }
+    }
+}
