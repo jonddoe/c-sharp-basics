c16f48b63ce0446cd28d27daca5f4e94c428b7ff
jonddoe <jandirkalman@gmail.com>
2021-04-27 18:39:40 +0300

    WordCount changed

--- Collections/WordCount/Program.cs
+++ Collections/WordCount/Program.cs
@@ -8,21 +8,16 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            string textRaw;
-            using (var sr = new StreamReader("../WordCount/lear.txt"))
-            {
-                textRaw = sr.ReadToEnd();
-            }
+            var readText = File.ReadAllLines("D:\\VS Projects\\c-sharp-basics\\Collections\\WordCount\\lear.txt");
 
-            var textSplitWithSpace = Regex.Replace(textRaw, @"(\s)\s+", " ");
-
-            var lineArray = textRaw.Split('\n');
+            var textSplitWithSpace = Regex.Replace(string.Join(" ", readText), @"(\s)\s+", " ");
             var wordArray = textSplitWithSpace.Split(' ');
-            var charArray = textSplitWithSpace.ToCharArray();
+            var charArray = string.Join("", readText).ToCharArray();
 
-            Console.WriteLine($"Lines = {lineArray.Length}");
+            Console.WriteLine($"Lines = {readText.Length}");
             Console.WriteLine($"Words = {wordArray.Length}");
             Console.WriteLine($"Chars = {charArray.Length}");
+
             Console.Read();
         }
     }
