9ae7ed2b7a5430afd4dd7d5f39d1b81c1b703126
jonddoe <79166658+jonddoe@users.noreply.github.com>
2021-05-07 18:13:42 +0300

    Exercise 4 Firm done

--- Polymorphism/Firm/Employee.cs
+++ Polymorphism/Firm/Employee.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Firm
 {
     public class Employee : StaffMember
@@ -8,28 +6,20 @@ namespace Firm
 
         protected double payRate;
 
-        //-----------------------------------------------------------------
-        // Sets up an employee with the specified information.
-        //-----------------------------------------------------------------
-        public Employee(string eName, string eAddress, string ePhone,
-            string socSecNumber, double rate) : base(eName, eAddress, ePhone)
+        public Employee(string eName, string eAddress, string ePhone, string socSecNumber, double rate) : base(eName,
+            eAddress, ePhone)
         {
             socialSecurityNumber = socSecNumber;
             payRate = rate;
         }
 
-        //-----------------------------------------------------------------
-        // Returns information about an employee as a string.
-        //-----------------------------------------------------------------
-         public override string ToString() {
-             var result = base.ToString();
-             result += "\nSocial Security Number: " + socialSecurityNumber;
-             return result;
-         }
+        public override string ToString()
+        {
+            var result = base.ToString();
+            result += "\nSocial Security Number: " + socialSecurityNumber;
+            return result;
+        }
 
-        //-----------------------------------------------------------------
-        // Returns the pay rate for this employee.
-        //-----------------------------------------------------------------
         public override double Pay()
         {
             return payRate;
--- Polymorphism/Firm/StaffMember.cs
+++ Polymorphism/Firm/StaffMember.cs
@@ -6,29 +6,21 @@ namespace Firm
         private string address;
         private string phone;
 
-        //-----------------------------------------------------------------
-        // Sets up a staff member using the specified information.
-        //-----------------------------------------------------------------
-        protected StaffMember(string eName, string eAddress, string ePhone) {
+        protected StaffMember(string eName, string eAddress, string ePhone)
+        {
             name = eName;
             address = eAddress;
             phone = ePhone;
         }
 
-        //-----------------------------------------------------------------
-        // Returns a string including the basic employee information.
-        //-----------------------------------------------------------------
-        public override string ToString() {
+        public override string ToString()
+        {
             var result = "Name: " + name + "\n";
             result += "Address: " + address + "\n";
             result += "Phone: " + phone;
             return result;
         }
 
-        //-----------------------------------------------------------------
-        // Derived classes must define the pay method for each type of
-        // employee.
-        //-----------------------------------------------------------------
         public abstract double Pay();
     }
 }
