aa93060fed9170ad356c02c75c41041cb46bac4c
jonddoe <79166658+jonddoe@users.noreply.github.com>
2021-05-18 18:16:49 +0300

    ArraysAndCollectionsTests added

--- ArraysAndCollectionsTests/ArraysAndCollectionsTests/WordCount.Tests/WordCountTests.cs
+++ ArraysAndCollectionsTests/ArraysAndCollectionsTests/WordCount.Tests/WordCountTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Xunit;
+using Assert = Xunit.Assert;
 
 namespace WordCount.Tests
 {
@@ -8,8 +9,14 @@ namespace WordCount.Tests
     public class WordCountTests
     {
         [Fact]
-        public void TestMethod1()
+        public void Generates2RandomValueArrays_ReturnsMinus7AtTheEndOfFirst()
         {
+            //arrange
+            const string expected = "-7";
+            //act
+            var actual = WordCount.LineWordCharCount();
+            //assert
+            Assert.Equal(expected, actual.Substring(actual.IndexOf("\n", StringComparison.Ordinal) - 2, 2));
         }
     }
 }
